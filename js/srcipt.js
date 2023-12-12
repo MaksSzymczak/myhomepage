2bc18b975865f50c9f38bb270781cead206e7768
Maksymilian Szymczak <maks.frontend@icloud.com>
2023-12-12 20:38:21 +0100

    Initial commit

--- js/srcipt.js
+++ js/srcipt.js
@@ -1,21 +1,32 @@
-console.log("Cześć fajnie ze jesteś😁")
+console.log("Cześć fajnie, że jesteś! 😁");
 let button = document.querySelector(".button");
 let body = document.querySelector(".body");
-let imageButton = document.querySelector(".imageButton");
+let js__imageButton = document.querySelector(".js__imageButton");
 let buttonSpan = document.querySelector(".buttonSpan");
 let image = document.querySelector(".image");
-let naviLink = document.querySelector(".naviLink");
-imageButton.addEventListener("click", () => {
-    image.remove();
-    imageButton.remove();
-})
+let image__container = document.querySelector(".image__container");
+
+js__imageButton.addEventListener("click", () => {
+  if (js__imageButton.innerText === "Usuń zdjęcie") {
+    // Usuń zdjęcie
+    if (image) {
+      image.remove();
+    }
+    // Przełącz tekst na przycisku
+    js__imageButton.innerText = "Dodaj zdjęcie";
+  } else {
+    // Dodaj zdjęcie
+    let clonedImage = image.cloneNode(true);
+    image__container.appendChild(clonedImage);
+    // Przełącz tekst na przycisku
+    js__imageButton.innerText = "Usuń zdjęcie";
+  }
+});
 
 button.addEventListener("click", () => {
-   
-    
-    body.classList.toggle("black");
-    buttonSpan.innerText = body.classList.contains("black") ? "jasny" : "szary"
-})
+  body.classList.toggle("black");
+  buttonSpan.innerText = body.classList.contains("black") ? "jasny" : "szary";
+});
 
 let links = document.querySelectorAll(".naviLink");
 
